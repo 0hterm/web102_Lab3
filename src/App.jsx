9ce85f59ce8f49460458b2207052a7bd84047142
Hunter Termo <huntertermo@yahoo.com>
2024-02-11 10:27:42 -0500

    Update finished functionality and styling

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,11 @@ function App() {
   return (
     <div className="App">
       <div className='title-container'>
-        <h1 className='title'>On My Grind</h1>
-        <p>
+        <div className='logo-title'>
+          <img src='https://images.vexels.com/media/users/3/323382/isolated/preview/d857584213563a05e60472a0538a19c7-coffee-cup-cartoon-cozy.png' id='logo'/>
+          <h1 className='title'>On My Grind</h1>
+        </div>
+        <p id='description'>
           So you think you can barista? 
           Let's put that to the test...
         </p>
